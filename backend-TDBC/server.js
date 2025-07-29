cd29aac60a69205371490050cdab8f1aa1e6c6f7
Arif Billah <billaharif661@gmail.com>
2025-07-29 13:12:29 +0600

    আপডেট ফাইল পুশ করা হয়েছে

--- backend-TDBC/server.js
+++ backend-TDBC/server.js
@@ -30,7 +30,7 @@ app.use('/api/admin/reviews', reviewRoutes);
 // Serve frontend files
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend'));
-  app.get('*', (req, res) => { 
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'CourseDetails.html'));
   });
 }
